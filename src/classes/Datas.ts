1f44c3dc4acc5a48e734aead8de3cf04c470527a
Emmanuel Martin <em.martin456@gmail.com>
2025-09-08 14:04:46 +0200

    création données templates

---
+++ src/classes/Datas.ts
@@ -0,0 +1,81 @@
+import { HasHtmlFormat } from "../interfaces/HasHtmlFormat";
+
+export class Datas implements HasHtmlFormat {
+
+  constructor (
+    private documentType: string,
+    private firstName: string,
+    private lastName: string,
+    private address: string,
+    private country: string,
+    private town: string,
+    private zip: number,
+    private product: string,
+    private price: number,
+    private quantity: number,
+    private tva: number,
+    private date: Date,
+  ) {
+
+  }
+
+  htmlFormat()  {
+    return `<div class="row p-5">
+    <div class="col-md-6">
+        <h2 class="text-left">LOGO</h2>
+    </div>
+    <div class="col-md-6 text-right">
+        <p class="font-weight-bold mb-1">xxxxxxxxxxxxxxx<span class="font-weight-normal">${this.documentType}</span></p>
+        <p class="font-weight-bold mb-1">Date <span class="font-weight-normal">${this.documentType}</span></p>
+    </div>
+</div>
+
+<div class="row pb-5 p-5">
+    <div class="col-sm-6 text-left">
+        <p class="font-weight-bold">Entreprise de Toto</p>
+        <p class="mb-1">22 boulevard Moe Szyslak</p>
+        <p>75008 - Paris</p>
+        <p class="mb-1">Tél: 00.00.00.00.00</p>
+    </div>
+
+    <div class="col-sm-6 text-right">
+        <p class="font-weight-bold">Informations du client</p>
+        <p class="mb-1">Mr/Mme ${this.firstName} ${this.lastName}</p>
+        <p class="mb-1">${this.address}</p>
+        <p>${this.country}</p>
+        <p>${this.town}</p>
+        <p>${this.zip}</p>
+    </div>
+</div>
+
+<div class="row p-5">
+    <div class="col-md-12">
+        <table class="table">
+        <thead>
+            <tr>
+            <th class="border-0 text-uppercase small font-weight-bold">Produit/Service</th>
+            <th class="border-0 text-uppercase small font-weight-bold">Prix unitaire HT</th>
+            <th class="border-0 text-uppercase small font-weight-bold">Quantité</th>
+            <th class="border-0 text-uppercase small font-weight-bold">Total HT</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>${this.product}</td>
+            <td>${this.price} € HT</td>
+            <td>${this.quantity}</td>
+            <td>${this.tva} € HT</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+</div>
+
+<div class="d-flex flex-row-reverse bg-light p-4">
+    <div class="py-3 px-5">
+        <div class="mb-2">TOTAL TTC</div>
+        <div class="h2 font-weight-light">xxxxxxxxxxxxxxx €</div>
+    </div>
+</div>`
+  }
+}
---
+++ src/classes/FormInputs.ts
@@ -0,0 +1,85 @@
+import { HasHtmlFormat } from "../interfaces/HasHtmlFormat.js";
+import { Datas } from "../classes/Datas.js"
+
+export class FormInput {
+
+  form: HTMLFormElement;
+  type: HTMLSelectElement;
+  firstName: HTMLInputElement;
+  lastName: HTMLInputElement;
+  address: HTMLInputElement;
+  country: HTMLInputElement;
+  town: HTMLInputElement;
+  zip: HTMLInputElement;
+  product: HTMLInputElement;
+  price: HTMLInputElement;
+  quantity: HTMLInputElement;
+  tva: HTMLInputElement;
+
+
+  constructor () {
+    this.form = document.querySelector("#form") as HTMLFormElement;
+    this.type = document.querySelector("#type") as HTMLSelectElement;
+    this.firstName = document.getElementById("firstName") as HTMLInputElement;
+    this.lastName = document.getElementById("lastName") as HTMLInputElement;
+    this.address = document.getElementById("address") as HTMLInputElement;
+    this.country = document.getElementById("country") as HTMLInputElement;
+    this.town = document.getElementById("town") as HTMLInputElement;
+    this.zip = document.getElementById("zip") as HTMLInputElement;
+    this.product = document.getElementById("product") as HTMLInputElement;
+    this.price = document.getElementById("price") as HTMLInputElement;
+    this.quantity = document.getElementById("quantity") as HTMLInputElement;
+    this.tva = document.getElementById("tva") as HTMLInputElement;
+
+    // Listener
+    this.submitFormListener();
+
+  }
+
+  // Listeners
+
+  private submitFormListener (): void {
+    this.form.addEventListener("submit", this.handleFormSubmit.bind(this))
+  }
+
+  private handleFormSubmit (event: Event) {
+    event.preventDefault();
+
+    const inputs = this.inputDatas() // => Renvoie soit un Array, soit Undefined
+
+    if (Array.isArray(inputs)) {
+      const [type, firstName, lastName, address, country, town, zip, product, price, quantity, tva] = inputs;
+      console.log(type, firstName, lastName, address, country, town, zip, product, price, quantity, tva);
+
+      let docData: HasHtmlFormat;
+      let date: Date = new Date();
+
+      docData =  new Datas(type, firstName, lastName, address, country, town, zip, product, price, quantity, tva, date);
+      console.log(docData.htmlFormat());
+
+
+    }
+
+  }
+
+  private inputDatas (): [string, string, string, string, string, string, number, string, number, number, number] | void {
+    const type = this.type.value;
+    const firstName = this.firstName.value;
+    const lastName = this.lastName.value;
+    const address = this.address.value;
+    const country = this.country.value;
+    const town = this.town.value;
+    const zip = this.zip.valueAsNumber;
+    const product = this.product.value;
+    const price = this.price.valueAsNumber;
+    const quantity = this.quantity.valueAsNumber;
+    const tva = this.tva.valueAsNumber;
+
+    if ( zip > 0 && price > 0 && quantity > 0 && tva > 0 )  {
+      return [type, firstName, lastName, address, country, town, zip, product, price, quantity, tva]
+    }
+    alert("les valeurs numériques doivent etre > 0 !")
+    return;
+  }
+
+}
