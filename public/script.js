c9c2923e60741df302a56545c2173ad0b4e48f1e
Emmanuel Martin <em.martin456@gmail.com>
2025-09-08 12:15:29 +0200

    update

--- public/script.js
+++ public/script.js
@@ -1,2 +1,9 @@
-console.log("Hello World !");
+// console.log("Hello World !");
 export {};
+// const palindrome = (phrase: string):string => {
+//   phrase = phrase.toLowerCase().replace("/\s+/g", " ");
+//   let reversed = phrase.split("").reverse().join("");
+//   return phrase === reversed ? `C'est un palindrome` : `Ce n'est pas un palindrome`;
+// }
+// const result: string = palindrome("rotor");
+// console.log(result);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,11 @@
-console.log("Hello World !");
+// console.log("Hello World !");
+
+// const palindrome = (phrase: string):string => {
+//   phrase = phrase.toLowerCase().replace("/\s+/g", " ");
+//   let reversed = phrase.split("").reverse().join("");
+
+//   return phrase === reversed ? `C'est un palindrome` : `Ce n'est pas un palindrome`;
+// }
+
+// const result: string = palindrome("rotor");
+// console.log(result);
